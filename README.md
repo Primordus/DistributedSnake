9e72f462e2eeaffbcf6a232cec70f053c55cadd0
Luc Tielen <luc.tielen@gmail.com>
2015-02-08 13:26:07 +0100

    Updated README, minor additions to snake.js

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 Snake on multiple computers. Based on Erlang and Elixir.
 
-[TOC]
-
 # Starting snake
 
 1. Install dependencies with `mix deps.get`
@@ -14,11 +12,11 @@ Snake on multiple computers. Based on Erlang and Elixir.
 
 1. snake.erl: head + tail logic for the snake itself
 2. bug.erl: everything involving the bug in the game
-3. gpio.erl: easy to use API for controlling GPIO pins of the Raspberry Pi
-4. led.erl: controlling LED using GPIO pins
-5. button.erl: reading a button use GPIO pins
-6. input.erl: reading input from keyboard
-7. game_tick.erl: game counter logic
-8. msg_counter.erl: tool to showcase how many messages are being sent
-9. connect.erl: for connecting the Erlang nodes together.
-10. Supervisor tree
+3. led.erl: controlling LED using GPIO pins
+4. button.erl: reading a button use GPIO pins
+5. input.erl: reading input from keyboard
+6. game_tick.erl: game counter logic
+7. msg_counter.erl: tool to showcase how many messages are being sent
+8. connect.erl: for connecting the Erlang nodes together.
+9. Supervisor tree
+10. Add functions to snake.js to draw everything on screen.
--- priv/static/js/snake.js
+++ priv/static/js/snake.js
@@ -9,29 +9,44 @@ $(function() {
     // Websocket logic:
     socket.join("snake", {}, function(channel) {
         channel.on("draw_snake", function(msg) {
-            drawSnake(canvas, msg);
+            draw_snake(canvas, msg);
         });
 
         channel.on("draw_insect", function(msg) {
-            drawInsect(canvas, msg);
+            draw_insect(canvas, msg);
         });
 
         channel.on("score", function(msg) {
-            updateScore(msg);
+            update_score(msg);
         });
     });
 });
 
-function drawSnake(canvas, snake) {
-    // TODO
+
+// TODO use assign to store board # on the connection.
+
+var size = 20;
+
+// TODO maybe remove function and work with diffs to clear screen.
+function clear_screen(canvas) {
+    var c = $("#canvas").get(0); // TODO refactor this later.
+    canvas.fillStyle = "black";
+    canvas.fillRect(0, 0, c.width, c.height);
 }
 
-function drawInsect(canvas, insect) {
-    // TODO
-    // TODO use assign to store board # on the connection.
+function draw_snake(canvas, snake) {
+    canvas.fillStyle = snake.color;
+    canvas.fillRect(snake.x, snake.y, size, size);
+    canvas.fillStyle = "white";
+    canvas.rect(snake.x, snake.y, size, size);
 }
 
-function updateScore(score) { // score currently not used..
+function draw_insect(canvas, insect) {
+    canvas.fillStyle = "red";
+    canvas.fillRect(insect.x, insect.y, size, size);
+}
+
+function update_score(score) { // score currently not used..
     var currentScore = $("#score");
     var newScore = parseInt(currentScore.html()) + 1;
     currentScore.html(newScore);
